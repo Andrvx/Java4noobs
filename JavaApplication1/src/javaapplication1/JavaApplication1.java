54f63c1fab66b05ce50d1ddd4864c99def2a1f58
Andrvx <andreslasarte@gmail.com>
2023-05-22 21:55:36 -0300

    Ejercicios del encuentor 29-JAVA

---
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,66 @@
+/*
+ * te edito el comentario
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author andre
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     * @param False
+     */
+    public static void main(String[] args) {
+        // poniendo algo en pantalla
+        System.out.println("A ver qué onda");
+        System.out.println("parece que anduvo");
+        
+        String cadenaza = "anda o no?";
+            System.out.print("variable cadenaza: ");
+            System.out.println(cadenaza);
+        
+        double puntoFlotanteLargo = 0.5456;
+            System.out.print("variable puntoFlotanteLargo: ");
+            System.out.println(puntoFlotanteLargo);
+        
+        int entero = 45;
+            System.out.print("variable entero: ");
+            System.out.println(entero);
+            
+        char caracter = 'A';
+        System.out.print("variable caracter: ");
+            System.out.println(caracter);
+        
+        float puntoFlotanteCorto;
+            puntoFlotanteCorto = 3.1416f;
+            System.out.print("variable puntoFlotanteCorto: ");
+            System.out.println(puntoFlotanteCorto);
+            
+        boolean logico;
+            logico = false;
+            System.out.println(logico);
+            
+        long enteroGrande = 1000000;
+            System.out.println(enteroGrande);
+            
+        short enteroCorto = -4500;
+            System.out.println(enteroCorto);
+            
+        byte enteroCortito = -8;
+            System.out.println(enteroCortito);
+            
+            
+            
+    /* deteccion de errores pag. 13
+        int numero = 48;
+    double decimales = 3.55;
+    boolean bandera = false;
+    */ 
+        
+    }
+    
+    
+}

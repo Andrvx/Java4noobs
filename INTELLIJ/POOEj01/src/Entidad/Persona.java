fbde04055746112591d674c2f1b4d49db650d076
Andrvx <andreslasarte@gmail.com>
2023-06-08 21:57:20 -0300

    Priemr encuentro de POO - 039 - 08-06-23

---
+++ INTELLIJ/POOEj01/src/Entidad/Persona.java
@@ -0,0 +1,13 @@
+package Entidad;
+
+public class Persona {
+    public String nombre;
+    public String apellido;
+    public int edad;
+    public Persona(){
+    }
+    public Persona(String nombre){
+        this.nombre= nombre;
+    }
+
+}
---
+++ INTELLIJ/POOEj01/src/Main.java
@@ -0,0 +1,12 @@
+import Entidad.Persona;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Persona primeraPersona=new Persona();
+
+        Persona segundaPersona=new Persona("Josecito");
+
+        }
+    }
+}

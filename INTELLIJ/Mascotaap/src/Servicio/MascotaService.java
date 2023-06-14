aea573fc16df4d6209c6d59e4188723aef6693bd
Andrvx <andreslasarte@gmail.com>
2023-06-13 21:50:52 -0300

    tercer encuentro POO

---
+++ INTELLIJ/Mascotaap/src/Servicio/MascotaService.java
@@ -0,0 +1,45 @@
+package Servicio;
+
+import entidades.Mascota;
+
+import java.util.Scanner;
+
+public class MascotaService {
+    private Scanner leer=new Scanner(System.in).useDelimiter("\n");
+
+    /**
+     *
+     * @return
+     */
+    public Mascota crearMascota() {
+        Mascota nuevaMascota=new Mascota();
+
+        System.out.println("Ingrese el tipo de mascota");
+        nuevaMascota.setTipo(leer.next());
+
+        System.out.println("Ingrese el nombre de la mascota");
+        nuevaMascota.setNombre(leer.next());
+
+        System.out.println("Ingrese si tiene cola o no (Sí/No).");
+        String cola=leer.next();
+        if ((cola.equals("Si")) || (cola.equals("Sí")) || (cola.equals("si")) || (cola.equals("sí")) || (cola.equals("yes"))){
+            nuevaMascota.setCola(true);
+        } else {
+            nuevaMascota.setCola(false);
+        }
+
+        return nuevaMascota;
+    }
+
+    /**
+     *
+     * @param masco
+     */
+    public void mostrarMascota(Mascota masco){
+        System.out.println(masco.getNombre());
+        System.out.println(masco.getTipo());
+        System.out.println(masco.getCola());
+
+    }
+}
+
--- INTELLIJ/Mascotaap/src/mascotaap/Mascotaap.java
+++ INTELLIJ/Mascotaap/src/mascotaap/Mascotaap.java
@@ -1,6 +1,7 @@
 package mascotaap;
 
 
+import Servicio.MascotaService;
 import entidades.Mascota;
 import entidades.Usuario;
 
@@ -9,10 +10,11 @@ import java.util.Scanner;
 public class Mascotaap {
     public static void main(String[] args) {
 
-        Scanner leer=new Scanner(System.in);
+        Scanner leer = new Scanner(System.in);
 
-        Mascota m1= new Mascota("Perrazo", "perrito", "Alto Perro", "negro", 15 , true, "saltazanja");
+        Mascota m1 = new Mascota("Perrazo", "perrito", "Alto Perro", "negro", 15, true, "saltazanja");
 /*
+        //asignando valores desde el main
         Mascota m1 = new Mascota();
         m1.apodo="Perrito";
         m1.nombre="Perrazo";
@@ -21,15 +23,25 @@ public class Mascotaap {
         m1.color="Negro";
         m1.raza="Perro";
         m1.tipo="Perro";
-*/
-        System.out.println("m1 "+m1.getNombre()+" "+m1.getApodo()+" "+m1.getColor()+" "+m1.getTipo()+" "+m1.isCola()+" "+m1.getRaza()+" "+m1.getEdad());
+
+        //setters y getters
+        System.out.println("m1 " + m1.getNombre() + " " + m1.getApodo() + " " + m1.getColor() + " " + m1.getTipo() + " " + m1.isCola() + " " + m1.getRaza() + " " + m1.getEdad());
         System.out.println("Nombre nuevo:");
         m1.setNombre(leer.next());
         System.out.println("Edad nueva:");
         m1.setEdad(leer.nextInt());
         System.out.println("Apodo nuevo:");
         m1.setApodo(leer.next());
-        System.out.println("m1 "+m1.getNombre()+" "+m1.getApodo()+" "+m1.getColor()+" "+m1.getTipo()+" "+m1.isCola()+" "+m1.getRaza()+" "+m1.getEdad());
-        System.out.println("m1 "+m1.getCola());
-        }
+        System.out.println("m1 " + m1.getNombre() + " " + m1.getApodo() + " " + m1.getColor() + " " + m1.getTipo() + " " + m1.isCola() + " " + m1.getRaza() + " " + m1.getEdad());
+        System.out.println("m1 " + m1.getCola());
+*/
+        //creando mascota usando la clase service
+        MascotaService mascotServ = new MascotaService();
+        Mascota m3 = mascotServ.crearMascota();
+        //System.out.println("m3 " + m3.getNombre() + " " + m3.getApodo() + " " + m3.getColor() + " " + m3.getTipo() + " " + m3.isCola() + " " + m3.getRaza() + " " + m3.getEdad());
+
+        //usando método mostrar en service
+        mascotServ.mostrarMascota(m3);
+
     }
+}

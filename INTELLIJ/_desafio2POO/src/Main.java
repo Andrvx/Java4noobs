17425cf414dd59cfc5182ec0ff76ce4093b6e3c2
Andrvx <andreslasarte@gmail.com>
2023-07-11 21:29:12 -0300

    más avance con el desfío2

--- INTELLIJ/_desafio2POO/src/Main.java
+++ INTELLIJ/_desafio2POO/src/Main.java
@@ -1,13 +1,64 @@
+import entidades.Alquiler;
+import entidades.Pelicula;
 import javafx.util.converter.LocalDateStringConverter;
+import servicio.servicioAlquiler;
+import servicio.servicioPelicula;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.chrono.ChronoLocalDate;
+import java.util.Scanner;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+        Scanner leer=new Scanner(System.in);
+        Pelicula[] vectorPeliculas=new Pelicula[10];
+        Alquiler[] vectorAlquileres=new Alquiler[10];
+        servicioPelicula SP=new servicioPelicula();
+        servicioAlquiler SA=new servicioAlquiler();
 
+        System.out.println("*** Programa Videoclub de los '90 ***");
+
+        boolean exit=false;
+        int opcion;
+        do {
+            System.out.println("  - Menú -");
+            System.out.println("   1- Ingresar Película");
+            System.out.println("   2- Listar Películas");
+            System.out.println("   3- Buscar Películas por Título");
+            System.out.println("   4- Buscar Películas por Género");
+            System.out.println("   5- Crear Alquiler");
+            System.out.println("   6- Listar Alquileres");
+            System.out.println("   7- Buscar Alquileres por Fecha");
+            System.out.println("   8- Cerrar Alquiler");
+            System.out.println("   9- Salir");
+            System.out.println("");
+            opcion=leer.nextInt();
+
+            switch (opcion){
+                case 1:
+
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+                case 7:
+                    break;
+                case 8:
+                    break;
+                case 9:
+                    break;
+            }
+
+        } while (!exit);
     }
 }
--- INTELLIJ/_desafio2POO/src/servicio/servicioAlquiler.java
+++ INTELLIJ/_desafio2POO/src/servicio/servicioAlquiler.java
@@ -3,6 +3,9 @@ import servicio.servicioPelicula;
 import entidades.Alquiler;
 import entidades.Pelicula;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -37,21 +40,77 @@ public class servicioAlquiler {
                             if (vectorAlquileres[i].getPelicula().equals("")){
                                 System.out.println("Cargando alquiler");
                                 vectorAlquileres[i].setPelicula(vectorPelicula[opcion]);
-                                System.out.println("Fecha de inicio:");
-                                //vectorAlquileres[i].setFechaInicio();
+
+                                System.out.println("Fecha de inicio (DDMMAAAA):");
+                                String input = leer.next();
+                                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+                                LocalDate date = LocalDate.parse(input, formatter);
+
+                                vectorAlquileres[i].setFechaInicio(date);
+
+                                valid=true;
                             }
                         }
                     }
                 } else {
-
+                    System.out.println("Opción inválida");
                 }
             } catch (InputMismatchException error){
                 System.out.println("Opción inválida");
             }
 
         } while (!valid);
+        System.out.println("");
     }
     //listarAlquileres
+    public void listarAlquileres(Alquiler[] vectorAlquileres){
+        System.out.println("*** Lista de alquileres ***");
+        for (int i=0;i<10;i++){
+            System.out.println(vectorAlquileres[i].toString());
+        }
+        System.out.println("");
+    }
     //buscarAlquilerFecha
+    public void buscarAlquilerFecha(Alquiler[] vectorAlquileres){
+        System.out.println("*** Buscando alquileres por fecha ***");
+
+        System.out.println("Ingrese la fecha de inicio (DDMMAAAA):");
+        String input = leer.next();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        LocalDate date = LocalDate.parse(input, formatter);
+        for (int i=0; i<10; i++){
+            if (vectorAlquileres[i].getFechaInicio().isEqual(date)){
+                System.out.println((i+1)+" - "+vectorAlquileres[i].toString());
+            }
+        }
+        System.out.println("");
+    }
     //calcularIngreso
+    public void calcularIngreso(Alquiler[] vectorAlquileres){
+        System.out.println("*** Cerrar Alquiler ***");
+        System.out.println("Seleccione una opción");
+        listarAlquileres(vectorAlquileres);
+        int aux=leer.nextInt();
+        aux--;
+
+        LocalDate dateCierre=LocalDate.now();
+
+        System.out.println("--- Calculando Precio Total ---");
+        vectorAlquileres[aux].setFechaFin(LocalDate.now());
+
+        long precioTot=10;
+        long diasTot=vectorAlquileres[aux].getFechaInicio().until(vectorAlquileres[aux].getFechaFin(), ChronoUnit.DAYS);
+        if (diasTot>3){
+          precioTot= (long) (precioTot*(0.1*(diasTot-3)));
+        }
+        System.out.println("El precio total es $ "+precioTot);
+
+        Alquiler auxAlq=new Alquiler();
+        vectorAlquileres[aux]=auxAlq;
+
+        System.out.println("--- Alquiler Cerrado ---");
+        System.out.println("");
+
+
+    }
 }
